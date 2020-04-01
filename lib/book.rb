b63f859f4a5beb37f0c128dfb095e3a342f9c904
Samiylo Kryshu <skryshu@gmail.com>
2020-04-01 20:26:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,6 +3,15 @@ class Book
     @title = title 
   end
   
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  
 end
 
 Book.new ("And Then There Were None")
