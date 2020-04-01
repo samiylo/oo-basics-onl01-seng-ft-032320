7b2509368114a0a8c4456e123a9d68482ef7d698
Samiylo Kryshu <skryshu@gmail.com>
2020-04-01 20:31:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,6 +23,16 @@ class Book
     @page_count
   end
   
+  def genre=(input_genre)
+    @genre = input_genre
+  end
+  
+  def genre 
+    @genre 
+  end
+  
+    
+  
 end
 
 Book.new ("And Then There Were None")
