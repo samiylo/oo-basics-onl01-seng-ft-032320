d81bee1c0053eb2c3be851b077c0c43158128da9
Samiylo Kryshu <skryshu@gmail.com>
2020-04-01 20:45:51 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,43 @@
+class Book
+  def initialize(title)
+    @title = title 
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(input_genre)
+    @genre = input_genre
+  end
+  
+  def genre 
+    @genre 
+  end
+  
+  def turn_page 
+    puts "Flipping the page...wow, you read fast!"
+  end
+    
+  
+end
+
+Book.new ("And Then There Were None")
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(input_brand)
+    @brand = input_brand
+  end
+  
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+   
+  
+end
+    
